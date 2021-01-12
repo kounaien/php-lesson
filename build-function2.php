72e747e121f243b0b01096ca07181993a28c9045
kounaien <1127fujihiroto@gmail.com>
2021-01-12 23:51:20 +0900

    array_filter

--- build-function2.php
+++ build-function2.php
@@ -62,3 +62,18 @@ function ($n) { return $n * 1.1 ;},
 );
 print_r($newPrices);
 
+//array_fillter'$a, $b)
+$numbers = range(1, 10);
+
+$evenNumbers = array_filter(
+  $numbers, function($n) {
+    if ($n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+  // return ($n) => % 2 === 0
+);
+
+print_r($evenNumbers);
